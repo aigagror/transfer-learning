8a96e13e3897227541b0008f0bfdfec0b15bcf64
eddie <aigagror@gmail.com>
2021-02-25 10:00:27 -0600

    verbose=0

--- training.py
+++ training.py
@@ -154,4 +154,4 @@ def class_transfer_learn(args, strategy, ds_id):
         transfer_model.fit(postprocess(ds_train, args.fine_bsz, repeat=True),
                            validation_data=postprocess(ds_val, args.fine_bsz),
                            initial_epoch=args.linear_epochs, epochs=args.linear_epochs + args.fine_epochs,
-                           steps_per_epoch=args.epoch_steps)
+                           steps_per_epoch=args.epoch_steps, verbose=0)
